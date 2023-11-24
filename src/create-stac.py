43afb619e8900aa6d93fd367a1e2688cd1843910
Lena Reimann <lena.reimann@vu.nl>
2023-11-24 12:46:58 +0100

    more folders hazard

--- src/create-stac.py
+++ src/create-stac.py
@@ -13,7 +13,9 @@ tmp_dir.name = 'C:/Users/lrn238/Documents/GitHub/climate-risk-stac/'
 root_url = r'https://raw.githubusercontent.com/DirkEilander/climate-risk-stac/main/stac'
 stac_extensions=[]
 
-# create main catalog
+#%% create catalog folder structure %%#
+
+# create main catalog (level0)
 catalog = pystac.Catalog(
     id="climate-risk-data", 
     title="Climate Risk Data",
@@ -21,9 +23,7 @@ catalog = pystac.Catalog(
     stac_extensions=stac_extensions,
 )
 
-#%%
-# add child catalogs
-
+# subcatalog hazard (level1)
 catalog_h = pystac.Catalog(
     id="hazard",
     title="Hazard",
@@ -32,16 +32,7 @@ catalog_h = pystac.Catalog(
 )
 catalog.add_child(catalog_h)
 
-catalog_ev = pystac.Catalog(
-    id="exposure-vulnerability",
-    title="Exposure and Vulnerability",
-    description="Exposure and Vulnerability datasets",
-    stac_extensions=stac_extensions,
-)
-catalog.add_child(catalog_ev)
-
-
-# add subcatalogs
+# subcatalogs hazard profile (level2)
 catalog_h1 = pystac.Catalog(
     id="flood",
     title="Flood",
@@ -51,15 +42,75 @@ catalog_h1 = pystac.Catalog(
 catalog_h.add_child(catalog_h1)
 
 catalog_h2 = pystac.Catalog(
-    id="river flood",
-    title="River flood",
-    description="River flooding datasets",
+    id="precipitation",
+    title="Precipitation",
+    description="Precipitation-related datasets",
     stac_extensions=stac_extensions,
 )
-catalog_h1.add_child(catalog_h2)
+catalog_h.add_child(catalog_h2)
+
+catalog_h3 = pystac.Catalog(
+    id="temperature",
+    title="Temperature",
+    description="Temperature-related datasets",
+    stac_extensions=stac_extensions,
+)
+catalog_h.add_child(catalog_h3)
+
+catalog_h4 = pystac.Catalog(
+    id="pressure-wind",
+    title="Pressure and Wind",
+    description="Pressure- and Wind-related datasets",
+    stac_extensions=stac_extensions,
+)
+catalog_h.add_child(catalog_h4)
+
+catalog_h5 = pystac.Catalog(
+    id="environment",
+    title="Environmental degradation",
+    description="Environmental degradation datasets",
+    stac_extensions=stac_extensions,
+)
+catalog_h.add_child(catalog_h5)
+
+catalog_h6 = pystac.Catalog(
+    id="multi-hazard",
+    title="Multi-hazard",
+    description="Multi-hazard datasets",
+    stac_extensions=stac_extensions,
+)
+catalog_h.add_child(catalog_h6)
+
+# subcatalogs hazard type (level3)
+catalog_h11 = pystac.Catalog(
+    id="coastal-flood",
+    title="Coastal flood flood",
+    description="Coastal flood datasets",
+    stac_extensions=stac_extensions,
+)
+catalog_h1.add_child(catalog_h11)
+
+catalog_h12 = pystac.Catalog(
+    id="fluvial-flood",
+    title="Fluvial flood",
+    description="Fluvial flood datasets",
+    stac_extensions=stac_extensions,
+)
+catalog_h1.add_child(catalog_h12)
+
+catalog_h13 = pystac.Catalog(
+    id="pluvial-flood",
+    title="Pluvial flood",
+    description="Pluvial flood datasets",
+    stac_extensions=stac_extensions,
+)
+catalog_h1.add_child(catalog_h13)
 
 
-# add collections
+
+
+#%%
+# add dataset collections
 collection = pystac.Collection(
     id="aqueduct",
     title="Aqueduct flood hazard maps",
@@ -69,8 +120,17 @@ collection = pystac.Collection(
         temporal=pystac.TemporalExtent([[datetime.utcnow(), None]]),
     ),
 )
-catalog_h2.add_child(collection)
+catalog_h12.add_child(collection)
 
+#%%
+# exposure & vulnerability
+catalog_ev = pystac.Catalog(
+    id="exposure-vulnerability",
+    title="Exposure and Vulnerability",
+    description="Exposure and Vulnerability datasets",
+    stac_extensions=stac_extensions,
+)
+catalog.add_child(catalog_ev)
 
 
 
