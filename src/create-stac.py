2f353cd932acb73b7e8866a51c056c2eac64c99b
Lena Reimann <80408894+lena-reimann@users.noreply.github.com>
2024-02-06 10:58:03 +0100

    changes catalog structure

--- src/create-stac.py
+++ src/create-stac.py
@@ -41,21 +41,21 @@ catalog_h1 = pystac.Catalog(
 catalog_h.add_child(catalog_h1)
 
 # hazard type (level3)
-catalog_h11 = pystac.Catalog(
-    id="coastal-flood",
-    title="Coastal flooding",
-    description="Coastal flooding datasets",
-    stac_extensions=stac_extensions,
-)
-catalog_h1.add_child(catalog_h11)
+#catalog_h11 = pystac.Catalog(
+#    id="coastal-flood",
+#    title="Coastal flooding",
+#    description="Coastal flooding datasets",
+#    stac_extensions=stac_extensions,
+#)
+#catalog_h1.add_child(catalog_h11)
 
-catalog_h12 = pystac.Catalog(
-    id="fluvial-flood",
-    title="Fluvial flooding",
-    description="Fluvial flooding datasets",
-    stac_extensions=stac_extensions,
-)
-catalog_h1.add_child(catalog_h12)
+#catalog_h12 = pystac.Catalog(
+#    id="fluvial-flood",
+#    title="Fluvial flooding",
+#    description="Fluvial flooding datasets",
+#    stac_extensions=stac_extensions,
+#)
+#catalog_h1.add_child(catalog_h12)
 
 #catalog_h13 = pystac.Catalog(
 #    id="pluvial-flood",
@@ -74,21 +74,21 @@ catalog_h2 = pystac.Catalog(
 )
 catalog_h.add_child(catalog_h2)
 
-catalog_h21 = pystac.Catalog(
-    id="drought",
-    title="Drought",
-    description="Drought datasets",
-    stac_extensions=stac_extensions,
-)
-catalog_h2.add_child(catalog_h21)
+#catalog_h21 = pystac.Catalog(
+#    id="drought",
+#    title="Drought",
+#    description="Drought datasets",
+#    stac_extensions=stac_extensions,
+#)
+#catalog_h2.add_child(catalog_h21)
 
-catalog_h22 = pystac.Catalog(
-    id="snow",
-    title="Heavy snowfall",
-    description="Heavy snowfall datasets",
-    stac_extensions=stac_extensions,
-)
-catalog_h2.add_child(catalog_h22)
+#catalog_h22 = pystac.Catalog(
+#    id="snow",
+#    title="Heavy snowfall",
+#    description="Heavy snowfall datasets",
+#    stac_extensions=stac_extensions,
+#)
+#catalog_h2.add_child(catalog_h22)
 
 
 catalog_h3 = pystac.Catalog(
@@ -99,21 +99,21 @@ catalog_h3 = pystac.Catalog(
 )
 catalog_h.add_child(catalog_h3)
 
-catalog_h31 = pystac.Catalog(
-    id="heat-wave",
-    title="Heat wave",
-    description="Heat wave datasets",
-    stac_extensions=stac_extensions,
-)
-catalog_h3.add_child(catalog_h31)
+#catalog_h31 = pystac.Catalog(
+#    id="heat-wave",
+#    title="Heat wave",
+#    description="Heat wave datasets",
+#    stac_extensions=stac_extensions,
+#)
+#catalog_h3.add_child(catalog_h31)
 
-catalog_h32 = pystac.Catalog(
-    id="cold-wave",
-    title="Cold wave",
-    description="Cold wave datasets",
-    stac_extensions=stac_extensions,
-)
-catalog_h3.add_child(catalog_h32)
+#catalog_h32 = pystac.Catalog(
+#    id="cold-wave",
+#    title="Cold wave",
+#    description="Cold wave datasets",
+#    stac_extensions=stac_extensions,
+#)
+#catalog_h3.add_child(catalog_h32)
 
 
 catalog_h4 = pystac.Catalog(
@@ -124,21 +124,21 @@ catalog_h4 = pystac.Catalog(
 )
 catalog_h.add_child(catalog_h4)
 
-catalog_h41 = pystac.Catalog(
-    id="tropical-cyclone",
-    title="Tropical cyclone",
-    description="Tropical cyclone datasets",
-    stac_extensions=stac_extensions,
-)
-catalog_h4.add_child(catalog_h41)
+#catalog_h41 = pystac.Catalog(
+#    id="tropical-cyclone",
+#    title="Tropical cyclone",
+#    description="Tropical cyclone datasets",
+#    stac_extensions=stac_extensions,
+#)
+#catalog_h4.add_child(catalog_h41)
 
-catalog_h42 = pystac.Catalog(
-    id="extratropical-cyclone",
-    title="Extratropical cyclone",
-    description="Extratropical cyclone datasets",
-    stac_extensions=stac_extensions,
-)
-catalog_h4.add_child(catalog_h42)
+#catalog_h42 = pystac.Catalog(
+#    id="extratropical-cyclone",
+#    title="Extratropical cyclone",
+#    description="Extratropical cyclone datasets",
+#    stac_extensions=stac_extensions,
+#)
+#catalog_h4.add_child(catalog_h42)
 
 
 catalog_h5 = pystac.Catalog(
@@ -160,11 +160,9 @@ catalog_h.add_child(catalog_h6)
 
 
 
-
-
 #%%
-# add dataset collections
-collection12 = pystac.Collection(
+# add dataset collections (just as a test)
+collection1 = pystac.Collection(
     id="aqueduct",
     title="Aqueduct flood hazard maps",
     description="Aqueduct flood hazard maps for different return periods ...",
@@ -172,8 +170,9 @@ collection12 = pystac.Collection(
         spatial=pystac.SpatialExtent([[-180, -90, 180, 90]]),
         temporal=pystac.TemporalExtent([[datetime.utcnow(), None]]),
     ),
+    #keywords="Fluvial flood" # or something like this?
 )
-catalog_h12.add_child(collection12)
+catalog_h1.add_child(collection1)
 
 
 
@@ -197,38 +196,38 @@ catalog_ev1 = pystac.Catalog(
 catalog_ev.add_child(catalog_ev1)
 
 #  exp (level3)
-catalog_ev11 = pystac.Catalog(
-    id="population-number",
-    title="Population number",
-    description="Datasets of population numbers",
-    stac_extensions=stac_extensions,
-)
-catalog_ev1.add_child(catalog_ev11)
+#catalog_ev11 = pystac.Catalog(
+#    id="population-number",
+#    title="Population number",
+#    description="Datasets of population numbers",
+#    stac_extensions=stac_extensions,
+#)
+#catalog_ev1.add_child(catalog_ev11)
 
 #  vul (level3)
-catalog_ev12 = pystac.Catalog(
-    id="demographics",
-    title="Demographics",
-    description="Datasets of demographic characteristics",
-    stac_extensions=stac_extensions,
-)
-catalog_ev1.add_child(catalog_ev12)
+#catalog_ev12 = pystac.Catalog(
+#    id="demographics",
+#    title="Demographics",
+#    description="Datasets of demographic characteristics",
+#    stac_extensions=stac_extensions,
+#)
+#catalog_ev1.add_child(catalog_ev12)
 
-catalog_ev13 = pystac.Catalog(
-    id="socioeconomic",
-    title="Socioeconomic status",
-    description="Datasets of socioeconomic status",
-    stac_extensions=stac_extensions,
-)
-catalog_ev1.add_child(catalog_ev13)
+#catalog_ev13 = pystac.Catalog(
+#    id="socioeconomic",
+#    title="Socioeconomic status",
+#    description="Datasets of socioeconomic status",
+#    stac_extensions=stac_extensions,
+#)
+#catalog_ev1.add_child(catalog_ev13)
 
-catalog_ev14 = pystac.Catalog(
-    id="adaptive-capacity",
-    title="Adaptive capacity",
-    description="Datasets of adaptive capacity",
-    stac_extensions=stac_extensions,
-)
-catalog_ev1.add_child(catalog_ev14)
+#catalog_ev14 = pystac.Catalog(
+#    id="adaptive-capacity",
+#    title="Adaptive capacity",
+#    description="Datasets of adaptive capacity",
+#    stac_extensions=stac_extensions,
+#)
+#catalog_ev1.add_child(catalog_ev14)
 
 
 catalog_ev2 = pystac.Catalog(
@@ -239,21 +238,21 @@ catalog_ev2 = pystac.Catalog(
 )
 catalog_ev.add_child(catalog_ev2)
 
-catalog_ev21 = pystac.Catalog(
-    id="building-footprints",
-    title="Building footprints",
-    description="Datasets of building footprints",
-    stac_extensions=stac_extensions,
-)
-catalog_ev2.add_child(catalog_ev21)
+#catalog_ev21 = pystac.Catalog(
+#    id="building-footprints",
+#    title="Building footprints",
+#    description="Datasets of building footprints",
+#    stac_extensions=stac_extensions,
+#)
+#catalog_ev2.add_child(catalog_ev21)
 
-catalog_ev22 = pystac.Catalog(
-    id="building-characteristics",
-    title="Building characteristics",
-    description="Datasets of building characteristics (e.g. use type, material, heritage)",
-    stac_extensions=stac_extensions,
-)
-catalog_ev2.add_child(catalog_ev22)
+#catalog_ev22 = pystac.Catalog(
+#    id="building-characteristics",
+#    title="Building characteristics",
+#    description="Datasets of building characteristics (e.g. use type, material, heritage)",
+#    stac_extensions=stac_extensions,
+#)
+#catalog_ev2.add_child(catalog_ev22)
 
 
 catalog_ev3 = pystac.Catalog(
@@ -264,21 +263,21 @@ catalog_ev3 = pystac.Catalog(
 )
 catalog_ev.add_child(catalog_ev3)
 
-catalog_ev31 = pystac.Catalog(
-    id="infrastructure-footprints",
-    title="Infrastructure footprints",
-    description="Datasets of infrastructure footprints",
-    stac_extensions=stac_extensions,
-)
-catalog_ev3.add_child(catalog_ev31)
+#catalog_ev31 = pystac.Catalog(
+#    id="infrastructure-footprints",
+#    title="Infrastructure footprints",
+#    description="Datasets of infrastructure footprints",
+#    stac_extensions=stac_extensions,
+#)
+#catalog_ev3.add_child(catalog_ev31)
 
-catalog_ev32 = pystac.Catalog(
-    id="infrastructure-characteristics",
-    title="Infrastructure characteristics",
-    description="Datasets of infrastructure characteristics (e.g. type, accessibility)",
-    stac_extensions=stac_extensions,
-)
-catalog_ev3.add_child(catalog_ev32)
+#catalog_ev32 = pystac.Catalog(
+#    id="infrastructure-characteristics",
+#    title="Infrastructure characteristics",
+#    description="Datasets of infrastructure characteristics (e.g. type, accessibility)",
+#    stac_extensions=stac_extensions,
+#)
+#catalog_ev3.add_child(catalog_ev32)
 
 
 catalog_ev4 = pystac.Catalog(
@@ -289,24 +288,24 @@ catalog_ev4 = pystac.Catalog(
 )
 catalog_ev.add_child(catalog_ev4)
 
-catalog_ev41 = pystac.Catalog(
-    id="urban-built",
-    title="Urban, built-up land",
-    description="Datasets of urban landuse and built-up land",
-    stac_extensions=stac_extensions,
-)
-catalog_ev4.add_child(catalog_ev41)
+#catalog_ev41 = pystac.Catalog(
+#    id="urban-built",
+#    title="Urban, built-up land",
+#    description="Datasets of urban landuse and built-up land",
+#    stac_extensions=stac_extensions,
+#)
+#catalog_ev4.add_child(catalog_ev41)
 
-catalog_ev42 = pystac.Catalog(
-    id="lulc",
-    title="Land use and land cover",
-    description="Land use and land cover datasets (e.g. forests, agriculture, protected land)",
-    stac_extensions=stac_extensions,
-)
-catalog_ev4.add_child(catalog_ev42)
+#catalog_ev42 = pystac.Catalog(
+#    id="lulc",
+#    title="Land use and land cover",
+#    description="Land use and land cover datasets (e.g. forests, agriculture, protected land)",
+#    stac_extensions=stac_extensions,
+#)
+#catalog_ev4.add_child(catalog_ev42)
 
 # add dataset collections
-collection11 = pystac.Collection(
+collection1 = pystac.Collection(
     id="ghs-pop",
     title="Global Human Settlement Layer Population",
     description="The Global Human Settlement Layer Population (GHS-POP) datasets are available in two different coordinate systems and two spatial resolutions each for the years 1975-2030 in 5-year time intervals.",
@@ -314,8 +313,9 @@ collection11 = pystac.Collection(
         spatial=pystac.SpatialExtent([[-180, -90, 180, 90]]),
         temporal=pystac.TemporalExtent([[datetime.utcnow(), None]]),
     ),
+    #keywords="Population number" # or something like this?
 )
-catalog_ev11.add_child(collection11)
+catalog_ev1.add_child(collection1)
 
 # add item (Timothy :))
 
