1d4f20cc9aa57766a02d969db6d7141eb092b62b
Lena Reimann <80408894+lena-reimann@users.noreply.github.com>
2024-07-24 16:03:27 +0200

    add collection

--- src/create-stac.py
+++ src/create-stac.py
@@ -14,13 +14,31 @@ dir = 'C:/Users/lrn238/OneDrive - Vrije Universiteit Amsterdam/Documents/GitHub/
 
 # Function to parse year range
 def parse_year_range(year_str):
-    year_str = year_str.replace('now', '2024').replace('current', '2024')
+     # If the string contains a dash, it's a range
     if '-' in year_str:
-        start, end = map(int, year_str.split('-'))
-        return datetime(start, 1, 1), datetime(end, 12, 31)
+        start, end = year_str.split('-')
+        
+        # Determine the length of the start and end strings to parse correctly
+        if len(start) == 4 and len(end) == 4:
+            start_year, end_year = int(start), int(end)
+            return datetime(start_year, 1, 1), datetime(end_year, 12, 31)
+        elif len(start) == 4 and len(end) == 0:
+            start_year = int(start)
+            end_year = datetime.now().year
+            end_month = datetime.now().month
+            end_day = datetime.now().day
+            return datetime(start_year, 1, 1), datetime(end_year, end_month, end_day)
+        else:
+            raise ValueError("Invalid year range format")
+    
+    # If there's no dash, it's a single year
     else:
-        year = int(year_str)
-        return datetime(year, 1, 1), None
+        if len(year_str) == 4:
+            year = int(year_str)
+            return datetime(year, 1, 1), datetime(year, 12, 31)
+        else:
+            raise ValueError("Invalid year format")
+        
 
 # Read data sheets
 hazard = pd.read_csv('csv/hazard.csv', encoding='utf-8')
@@ -50,6 +68,7 @@ def create_catalog_from_csv(indicator, catalog_main, dir):
         title_collection = item['title_short'] if not pd.isna(item['title_short']) else item['title_collection']
         if title_collection is None: title_collection = item['title_item']
         
+        # bbox and temp resolution
         bbox = item['bbox']
         temporal_resolution = item['temporal_resolution']
         
@@ -61,61 +80,61 @@ def create_catalog_from_csv(indicator, catalog_main, dir):
             catalog1 = catalog_main.get_child(catalog_id)
 
         # Create or retrieve the second-level catalog
-        if category_id not in [cat.id for cat in catalog1.get_children()]: ############# check here!!!! ###########
+        if category_id not in [cat.id for cat in catalog1.get_children()]:
             catalog2 = pystac.Catalog(id=category_id, title=category_id, description=category_id) #adjust here once it works
             catalog1.add_child(catalog2)
         else:
             catalog2 = catalog1.get_child(category_id)   
         
 
-        # # Create or retrieve the collection ## change here!! ##
-        # if category_id not in [col.id for col in new_catalog.get_children()]:
-        #     # Process bbox
-        #     if not np.nan_to_num(bbox) == 0:
-        #         try:
-        #             bbox_list = [float(coord.strip()) for coord in bbox.split(',')]
-        #         except ValueError:
-        #             print(f'Check bbox: {bbox}')
-        #             bbox_list = bbox
-        #     else:
-        #         bbox_list = np.nan
+        # Create or retrieve the collection 
+        if title_collection not in [col.id for col in catalog2.get_children()]:
+            # Process bbox
+            if not np.nan_to_num(bbox) == 0:
+                try:
+                    bbox_list = [float(coord.strip()) for coord in bbox.split(',')]
+                except ValueError:
+                    print(f'Check bbox: {bbox}')
+                    bbox_list = bbox
+            else:
+                bbox_list = np.nan
 
-        #     # Process temporal resolution
-        #     if not np.nan_to_num(temporal_resolution) == 0:
-        #         year_start, year_end = parse_year_range(str(temporal_resolution))
-        #     else:
-        #         year_start, year_end = np.nan, np.nan
-        #         print('Cannot create collection, because of no input for temporal_resolution')
-        #         continue
+            # Process temporal resolution ## this needs to be changed to account for the total range of all items ##
+            if not np.nan_to_num(temporal_resolution) == 0:
+                year_start, year_end = parse_year_range(str(temporal_resolution))
+            else:
+                year_start, year_end = np.nan, np.nan
+                print('Cannot create collection, because of no input for temporal_resolution')
+                continue
 
-        #     keywords = item['keywords'].split(',') if 'keywords' in item else []
+            #keywords = item['keywords'].split(',') if 'keywords' in item else [] ## check for proper formatting of keywords
             
-        #     collection = pystac.Collection(
-        #         id=category_id,
-        #         title=title_collection,
-        #         description=title_collection,
-        #         extent=pystac.Extent(
-        #             spatial=pystac.SpatialExtent([bbox_list]),
-        #             temporal=pystac.TemporalExtent([[year_end, year_start]]),
-        #         ),
-        #         extra_fields={
-        #             'data_type': item['data_type'],
-        #             'data_format': item['format'],
-        #             'spatial_scale': item['spatial_scale'],
-        #             'coordinate_system': item['coordinate_system'],
-        #             'keywords': keywords
-        #         },
-        #     )
-        #     new_catalog.add_child(collection)
-        # else:
-        #     collection = new_catalog.get_child(category_id)
+            collection = pystac.Collection(
+                id=title_collection,
+                title=title_collection,
+                description=title_collection, #adjust here
+                extent=pystac.Extent(
+                    spatial=pystac.SpatialExtent([bbox_list]),
+                    temporal=pystac.TemporalExtent([[year_start, year_end]]),
+                ),
+                extra_fields={
+                    'type': item['subcategory']
+                    #'keywords': keywords
+                },
+            )
+            catalog2.add_child(collection)
+        else:
+            collection = catalog2.get_child(title_collection)
         
-        # # Create the item
+        #Create the item
+        # year_start, year_end = parse_year_range(str(temporal_resolution))
+        # datetime_value = year_start if year_start else datetime.now()
+
         # item_stac = pystac.Item(
         #     id=item['title_item'],
         #     geometry=None,  # Add geometry if available
         #     bbox=bbox_list,
-        #     datetime=None,
+        #     datetime=year_start,
         #     properties={
         #         'title': item['title_item'],
         #         'description': item['description_item'],
@@ -157,4 +176,4 @@ def create_catalog_from_csv(indicator, catalog_main, dir):
 
 # Create catalogs from both hazard and exposure-vulnerability CSVs
 create_catalog_from_csv(hazard, catalog_main, dir)
-create_catalog_from_csv(expvul, catalog_main, dir)
+#create_catalog_from_csv(expvul, catalog_main, dir)
