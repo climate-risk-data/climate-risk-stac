7d4ab24b5ac075b39f6f20c5274009990eb40055
Lena Reimann <80408894+lena-reimann@users.noreply.github.com>
2024-07-25 12:08:31 +0200

    Update create-stac.py

--- src/create-stac.py
+++ src/create-stac.py
@@ -116,9 +116,9 @@ def create_catalog_from_csv(indicator, catalog_main, dir):
             #keywords = item['keywords'].split(',') if 'keywords' in item else [] ## check for proper formatting of keywords
             
             collection = pystac.Collection(
-                id='test', #title_collection,
-                title='test',#title_collection,
-                description='test', #title_collection, #adjust here
+                id=title_collection, #'test', #
+                title=title_collection, #'test',#
+                description=title_collection, #'test', ##adjust here
                 extent=pystac.Extent(
                     spatial=pystac.SpatialExtent([bbox_list]),
                     temporal=pystac.TemporalExtent([[year_start, year_end]]),
