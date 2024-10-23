e5c2e924be5adcb28a292e8b2423d49a299b268a
Lena Reimann <80408894+lena-reimann@users.noreply.github.com>
2024-10-23 11:34:20 +0200

    change keywords function for subcategory keywords

--- climate_stac/utils.py
+++ climate_stac/utils.py
@@ -91,9 +91,10 @@ def generate_keywords(item: pystac.Item) -> list:
     risk_data = item['risk_data_type'] if item['risk_data_type'] != 'hazard' else None
     
     # Subcategory: a) replace subcategory with string with commas, b) split the string if it contains commas
-    if item['category'] == 'flood' and item['subcategory'] == 'flood type not specified':
-        item['subcategory'] = "coastal flood,fluvial flood,pluvial flood"
-    subcategories = item['subcategory'].split(',') if ',' in item['subcategory'] else [item['subcategory']]
+    #if item['category'] == 'flood' and item['subcategory'] == 'flood type not specified':
+    #    item['subcategory'] = "coastal flood,fluvial flood,pluvial flood"
+    subcat = item['subcategory'] if np.nan_to_num(item['subcategory']) else None
+    subcategories = subcat.split(',') if ',' in subcat else subcat
     
     # Extract other attributes
     spatial_scale = item['spatial_scale']
